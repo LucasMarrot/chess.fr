9fe99e855146ad94fc40884048c2865681cdfeab
Lucas Marrot <lmarrot@iutbayonne.univ-pau.fr>
2026-06-14 19:47:14 +0200

    fix: update button label from 'Revenir a la Home' to 'Quitter' in LocalGameResultOverlay

--- components/chess/local-game/LocalGameResultOverlay.tsx
+++ components/chess/local-game/LocalGameResultOverlay.tsx
@@ -65,7 +65,7 @@ export const LocalGameResultOverlay = ({
               textAlign: 'center',
             }}
           >
-            Revenir a la Home
+            Quitter
           </ChessButton>
         </>
       }
